5030e74114c52f09f7ae8e27c27cbb808cda5c9b
peytonlynnbarnes <peyton0330@gmail.com>
2026-03-12 18:06:02 -0500

    dialogic base project running

---
+++ Assets/Scripts/DialogueController.gd
@@ -0,0 +1,12 @@
+extends Node
+
+func dynamic_dialogue() -> void:
+	var lines = [
+		"Fresh moonberries!",
+		"Careful, those glow.",
+        "Straight from the asteroid farms."
+	]
+	Global.dynamic_line = lines.pick_random()
+	
+func static_dialogue():
+	Global.set_variable("dynamic_line", "Fresh alien fruit!")
---
+++ Assets/Scripts/Global.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var dynamic_line: String = "123"
+var player_input: String = ""
