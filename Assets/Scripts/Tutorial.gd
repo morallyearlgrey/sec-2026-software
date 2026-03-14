f57e6c99b4adfe9383524717e9ae7d856eacaeef
peytonlynnbarnes <peyton0330@gmail.com>
2026-03-13 23:25:03 -0500

    music and art

--- Assets/Scripts/Tutorial.gd
+++ Assets/Scripts/Tutorial.gd
@@ -1,13 +1,11 @@
 extends Control
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func _on_start_button_pressed() -> void:
+func _on_yes_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Assets/Scenes/Map.tscn")
+
+func _on_no_button_pressed() -> void:
+	Dialogic.start("tutorial")
